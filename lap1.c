8d162d32305c1eb9f7b5c2a2795a8ba4692d4bff
6704062611301TK <s6704062611301@email.kmutnb.ac.th>
2024-12-03 13:26:28 +0700

    Add files via upload

--- lap1.c
+++ lap1.c
@@ -1,8 +1,8 @@
 int main(){
 	int num1;
 	int num2;
-	scanf("%d",&num1);
-	scanf("%d",&num2);
+	scanf("%d\n",&num1);
+	scanf("%d\n",&num2);
 	if(num1 > num2){
 		printf("num1 is greater than num2");
 	} else
